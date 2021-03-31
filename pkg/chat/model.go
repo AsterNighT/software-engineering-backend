29b9f5bc18b36f0372e95933983dd39777093ae5
YJ Q <1441158517@qq.com>
2021-03-31 10:16:07 +0800

    Add chat
    Signed-off-by: YJ Q <1441158517@qq.com>

---
+++ pkg/chat/model.go
@@ -0,0 +1,32 @@
+package chat
+
+type Chat struct {
+	ID        uint
+	URL       string //The url of the chatting window
+	DoctorID  uint
+	PatientID uint
+
+	//primary key: ID
+}
+
+type Message struct {
+	ID         uint
+	ChatID     uint
+	Timestamp  string
+	Type       uint //Texts, pictures....
+	Content    string
+	PictureURL string
+
+	//primary key: ID&chatID
+}
+
+//Question catagories
+//For standard query,each catagory correspond with a set of fixed questions
+type Catagory struct {
+	ID        uint
+	Name      string
+	Keywords  []string
+	Questions []string
+
+	//primary key: ID
+}
