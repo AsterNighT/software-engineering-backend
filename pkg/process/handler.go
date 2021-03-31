7cfb689a72423cff904489024b56dec0efd85869
PAN-Ziyue <alphavake2018@gmail.com>
2021-03-31 23:16:45 +0800

    update G4's backend model design

--- pkg/process/handler.go
+++ pkg/process/handler.go
@@ -21,16 +21,14 @@ func (h *RegistrationHandler) GetAllDepartments(c echo.Context) error {
 	return c.JSON(http.StatusCreated, api.Return("pong", nil))
 }
 
-
-
-// @Summary get all departments
+// @Summary get a department by its ID
 // @Tags departments
-// @Description display all departments of a hospital
+// @Description return a department's details by its ID
 // @Param department_id path uint true "Department ID"
 // @Produce json
 // @Success 200 {object} api.ReturnedData{data=Department}
-// @Router /departments [GET]
-func (h *RegistrationHandler) GetDepartment(c echo.Context) error {
+// @Router /departments/{department_id} [GET]
+func (h *RegistrationHandler) GetDepartmentByID(c echo.Context) error {
 
 	c.Logger().Debug("hello world")
 	return c.JSON(http.StatusCreated, api.Return("pong", nil))
