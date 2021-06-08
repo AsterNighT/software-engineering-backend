edb0af7ad45e612d417cda3fd94f4eed3f78a6bc
rupertpaoz <1589669635@qq.com>
2021-06-08 20:24:39 +0800

    fixed bugs in cases and test passed

--- pkg/cases/handler.go
+++ pkg/cases/handler.go
@@ -17,8 +17,8 @@ type MedicineHandler struct {
 // @Tags Case
 // @Produce json
 // @Param department query string false "department name" nil
-// @Param patient query string false "patient name" nil
-// @Param doctor query string false "doctor name" nil
+// @Param patientID query uint false "patient ID" nil
+// @Param doctorID query uint false "doctor ID" nil
 // @Param before query uint false "a timestamp marking end time" nil
 // @Param after query uint false "a timestamp marking start time" nil
 // @Param q query string false "full-text search" nil
@@ -26,19 +26,19 @@ type MedicineHandler struct {
 // @Router /cases [GET]
 func (h *CaseHandler) GetAllCases(c echo.Context) error {
 	db := utils.GetDB()
-	params := []string{
-		"department",
-		"patient",
-		"doctor",
+
+	if c.QueryParam(("patientID")) != "" {
+		db = db.Where("patient_id = ?", c.QueryParam("patientID"))
 	}
-	for _, s := range params {
-		if c.QueryParam(s) != "" {
-			db.Where("? LIKE ?", s, "%"+c.QueryParam(s)+"%")
-		}
+	if c.QueryParam(("doctorID")) != "" {
+		db = db.Where("doctor_id = ?", c.QueryParam("doctorID"))
+	}
+	if c.QueryParam("department") != "" {
+		db = db.Where("department LIKE ?", "%"+c.QueryParam("department")+"%")
 	}
 
 	var cases []Case
-	db.Find(&cases)
+	db.Preload("Prescriptions").Preload("Prescriptions.Guidelines").Preload("Prescriptions.Guidelines.Medicine").Find(&cases)
 
 	c.Logger().Debug("GetAllCases")
 	return c.JSON(200, api.Return("ok", cases))
@@ -53,10 +53,10 @@ func (h *CaseHandler) GetAllCases(c echo.Context) error {
 // @Router /patient/{patientID}/case [GET]
 func (h *CaseHandler) GetLastCaseByPatientID(c echo.Context) error {
 	db := utils.GetDB()
-	db.Where("PatientID = ?", c.Param("patientID"))
+	db = db.Where("patient_id = ?", c.Param("patientID"))
 
 	var case1 Case
-	db.Last(&case1)
+	db.Preload("Prescriptions").Preload("Prescriptions.Guidelines").Preload("Prescriptions.Guidelines.Medicine").Last(&case1)
 
 	c.Logger().Debug("GetLastCase")
 	return c.JSON(200, api.Return("ok", case1))
@@ -76,10 +76,10 @@ func (h *CaseHandler) GetLastCaseByPatientID(c echo.Context) error {
 // @Router /patient/{patientID}/cases [GET]
 func (h *CaseHandler) GetCasesByPatientID(c echo.Context) error {
 	db := utils.GetDB()
-	db.Where("PatientID = ?", c.Param("patientID"))
+	db = db.Where("patient_id = ?", c.Param("patientID"))
 
-	var cases Case
-	db.Find(&cases)
+	var cases []Case
+	db.Preload("Prescriptions").Preload("Prescriptions.Guidelines").Preload("Prescriptions.Guidelines.Medicine").Find(&cases)
 
 	c.Logger().Debug("GetCasesByPatientID")
 	return c.JSON(200, api.Return("ok", cases))
@@ -179,6 +179,7 @@ func (h *CaseHandler) NewPrescription(c echo.Context) error {
 	if err != nil {
 		return c.JSON(400, api.Return("error", err))
 	}
+
 	result := db.Create(&pre)
 	if result.Error != nil {
 		return c.JSON(400, api.Return("error", result.Error))
@@ -233,7 +234,12 @@ func (h *CaseHandler) UpdatePrescription(c echo.Context) error {
 func (h *CaseHandler) GetPrescriptionByPrescriptionID(c echo.Context) error {
 	db := utils.GetDB()
 	var pre Prescription
-	db.First(&pre, c.Param("prescriptionID"))
+	db.Preload("Guidelines").Preload("Guidelines.Medicine").First(&pre, c.Param("prescriptionID"))
+
+	// var jsonStu []byte
+	// jsonStu, _ = json.Marshal(pre)
+	// fmt.Println(string(jsonStu))
+
 	c.Logger().Debug("GetPrescriptionByPrescriptionID")
 	return c.JSON(200, api.Return("ok", pre))
 }
@@ -247,9 +253,8 @@ func (h *CaseHandler) GetPrescriptionByPrescriptionID(c echo.Context) error {
 // @Router /patient/{patientID}/case/{caseID}/prescription  [GET]
 func (h *CaseHandler) GetPrescriptionByCaseID(c echo.Context) error {
 	db := utils.GetDB()
-	db.Where("CaseID = ?", c.Param("caseID"))
 	var pres []Prescription
-	db.Find(&pres)
+	db.Where("CaseID = ?", c.Param("caseID")).Preload("Guidelines").Preload("Guidelines.Medicine").Find(&pres)
 	c.Logger().Debug("GetPrescriptionByCaseID")
 	return c.JSON(200, api.Return("ok", pres))
 }
@@ -263,9 +268,8 @@ func (h *CaseHandler) GetPrescriptionByCaseID(c echo.Context) error {
 // @Router /medicine [GET]
 func (h *MedicineHandler) GetMedicines(c echo.Context) error {
 	db := utils.GetDB()
-	db.Where("name = ?", c.QueryParam("q"))
 	var meds []Medicine
-	db.Find(&meds)
+	db.Where("name LIKE ?", "%"+c.QueryParam("q")+"%").Find(&meds)
 	c.Logger().Debug("QueryMedicine")
 	return c.JSON(200, api.Return("ok", meds))
 }
