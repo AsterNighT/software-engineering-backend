596bbff7be723970045ca3d20698cf38fd2bbedd
rupertpaoz <1589669635@qq.com>
2021-06-20 23:33:10 +0800

    change some ID type from string to uint

--- pkg/cases/utils.go
+++ pkg/cases/utils.go
@@ -8,7 +8,7 @@ import (
 
 func FromAdmin(c echo.Context) bool {
 	db, _ := c.Get("db").(*gorm.DB)
-	id := c.Get("id").(string)
+	id := c.Get("id").(uint)
 	var user account.Account
 	if err := db.Where("id = ?", id).First(&user).Error; err == nil { // not found
 		return user.Type == account.AdminType
@@ -21,7 +21,7 @@ func FromDoctor(c echo.Context) bool {
 		return true
 	}
 	db, _ := c.Get("db").(*gorm.DB)
-	id := c.Get("id").(string)
+	id := c.Get("id").(uint)
 	var user account.Account
 	if err := db.Where("id = ?", id).First(&user).Error; err == nil { // not found
 		return user.Type == account.DoctorType
@@ -29,11 +29,11 @@ func FromDoctor(c echo.Context) bool {
 	return false
 }
 
-func FromPatient(c echo.Context, id string) bool {
+func FromPatient(c echo.Context, id uint) bool {
 	if FromDoctor(c) {
 		return true
 	}
-	if c.Get("id").(string) == id {
+	if c.Get("id").(uint) == id {
 		return true
 	}
 	c.Logger().Errorf("unauthorized access, expecting user with id:%s", id)
