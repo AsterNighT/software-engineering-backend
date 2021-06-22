0a48a9de9955e1ec5a681df88543a04865e95732
Scott Chen <48900010+FUTURETECH6@users.noreply.github.com>
2021-06-22 15:33:25 +0000

    fix: ci lint

--- pkg/account/handler.go
+++ pkg/account/handler.go
@@ -465,7 +465,7 @@ func ParseToken(tokenString string) (uint, error) {
 	}
 	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(os.Getenv("JWT_KEY")), nil
 	})
