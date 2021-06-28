0355957cf616e12f866ed9ba82def2dce87fa8ea
Ziyue-PAN <ziyuepan99@outlook.com>
2021-06-28 09:44:16 +0800

    feat: add a init function for departments

--- pkg/database/init.go
+++ pkg/database/init.go
@@ -5,12 +5,14 @@ import (
 	"github.com/AsterNighT/software-engineering-backend/pkg/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	//dsn := "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=neon-cubes.xyz user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	var db *gorm.DB
 	var err error
 
@@ -69,6 +71,8 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
+
+	initDepartment()
 	return db
 }
 
@@ -79,4 +83,43 @@ func ContextDB(db *gorm.DB) echo.MiddlewareFunc {
 			return next(c)
 		}
 	}
+}
+
+// initDepartment initialize the department tables.
+func initDepartment() {
+	departments := []models.Department{{
+		Name:   "眼科",
+		Detail: "世界一流眼科",
+	}, {
+		Name:   "骨科",
+		Detail: "",
+	}, {
+		Name:   "呼吸内科",
+		Detail: "",
+	}, {
+		Name:   "妇产科",
+		Detail: "专治不孕不育",
+	}, {
+		Name:   "肿瘤内科",
+		Detail: "",
+	}, {
+		Name:   "心胸外科",
+		Detail: "",
+	}, {
+		Name:   "耳鼻喉科",
+		Detail: "",
+	}, {
+		Name:   "急诊科",
+		Detail: "",
+	}}
+
+	db := utils.GetDB()
+	db.Clauses(clause.OnConflict{DoNothing: true}).Create(&departments)
+
+	//for i := range departments {
+	//	db.Clauses(clause.OnConflict{DoNothing: true}).Create(&models.Department{
+	//		Name: departments[i].Name,
+	//		Detail: departments[i].Detail,
+	//	})
+	//}
 }
